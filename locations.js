ead3b1a2669f17d08e806b9f1fca936a343b24bd
ben <mynameiswilson@users.noreply.github.com>
2011-05-02 12:17:44 +0000

    initial add of cycletour web interface

---
+++ locations.js
@@ -0,0 +1,23 @@
+var locations = [
+	{
+		"order": 1,
+		"shortname": "cars",
+		"name": "Cars"
+	},
+	{
+		"order": 2,
+		"shortname": "peloton-rear",
+		"name": "Peloton: Rear"
+	},
+	{
+		"order": 3,
+		"shortname": "peloton-middle",
+		"name": "Peloton: Middle"
+	},
+    {
+		"order": 4,
+		"shortname": "peloton-front",
+		"name": "Peloton: Head"
+	}, 
+	
+]
