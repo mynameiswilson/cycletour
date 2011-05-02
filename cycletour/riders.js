bb87dcb92c9b7f0ae5f7fdb484a3625e83a8150b
ben <ben@9a4b3800-4c9c-4e24-ae1c-011c3f90af91>
2011-05-02 12:17:44 +0000

    initial add of cycletour web interface

---
+++ cycletour/riders.js
@@ -0,0 +1,142 @@
+var riders = [
+    {
+        "id": 1,
+		"name": "Mark Cavendish",
+        "stats": { "flat": 8 ,
+        "downhill": 7,
+        "hill": 6 ,
+        "cobbles": 6,
+        "mountain": 4 ,
+        "sprint": 8 }
+    },
+    {
+        "id": 2,
+		"name": "Tony Martin",
+        "stats": { "flat": 8 ,
+        "downhill": 6 ,
+        "hill": 7 ,
+        "cobbles": 5,
+        "mountain": 7 ,
+        "sprint": 6 }
+    },
+    {
+        "id": 3,
+		"name": "Marco Pinotti",
+        "stats": { "flat": 6 ,
+        "downhill": 6,
+        "hill": 7 ,
+        "cobbles": 5,
+        "mountain": 7 ,
+        "sprint": 5 }
+    },
+    {
+        "id": 4,
+		"name": "Mark Renshaw",
+        "stats": { "flat": 8 ,
+        "downhill": 6 ,
+        "hill": 6 ,
+        "cobbles": 5,
+        "mountain": 4 ,
+        "sprint": 7 }
+    },
+    {
+        "id": 5,
+		"name": "Hayden Roulston",
+        "stats": { "flat": 7 ,
+        "downhill": 6 ,
+        "hill": 6 ,
+        "cobbles": 7,
+        "mountain": 5 ,
+        "sprint": 7 }
+    },
+    {
+        "id": 6,
+		"name": "Michael Albasini",
+        "stats": { "flat": 7 ,
+        "downhill": 6 ,
+        "hill": 8 ,
+        "cobbles": 6,
+        "mountain": 7 ,
+        "sprint": 5 }
+    },
+    {
+        "id": 7,
+		"name": "Bernhard Eisel",
+        "stats": { "flat": 8 ,
+        "downhill": 6 ,
+        "hill": 6 ,
+        "cobbles": 7,
+        "mountain": 5 ,
+        "sprint": 7 }
+    },
+    {
+        "id": 8,
+		"name": "Bert Grabsch",
+        "stats": { "flat": 8 ,
+        "downhill": 7 ,
+        "hill": 6 ,
+        "cobbles": 7,
+        "mountain": 6 ,
+        "sprint": 5 }
+    },
+    {
+        "id": 9,
+		"name": "Matt Goss",
+        "stats": { "flat": 8 ,
+        "downhill": 7 ,
+        "hill": 7 ,
+        "cobbles": 7,
+        "mountain": 5 ,
+        "sprint": 7 }
+    },
+    {
+        "id": 10, 
+		"name": "Konstant Sivtsov",
+        "stats": { "flat": 7 ,
+        "downhill": 6 ,
+        "hill": 7 ,
+        "cobbles": 5,
+        "mountain": 7 ,
+        "sprint": 5 }
+    },
+    {
+        "id": 11,
+		"name": "Andreas Klöden",
+        "stats": { "flat": 7 ,
+        "downhill": 6 ,
+        "hill": 8 ,
+        "cobbles": 5,
+        "mountain": 8 ,
+        "sprint": 5 }
+    },
+    {
+        "id": 12,
+		"name": "Robbie McEwen",
+        "stats": { "flat": 8 ,
+        "downhill": 7 ,
+        "hill": 5 ,
+        "cobbles": 7,
+        "mountain": 4 ,
+        "sprint": 8 }
+    },
+    {            
+		"id": 13,
+        "name": "Levi Leipheimer",
+        "stats": { "flat": 7 ,
+        "downhill": 6 ,
+        "hill": 7 ,
+        "cobbles": 5,
+        "mountain": 8 ,
+        "sprint": 4 }
+    },
+    {
+        "id": 14,
+		"name": "Fumiyuki Beppu",
+        "stats": { "flat": 7 ,
+        "downhill": 6 ,
+        "hill": 6 ,
+        "cobbles": 6,
+        "mountain": 6 ,
+        "sprint": 6 }
+    }
+]
